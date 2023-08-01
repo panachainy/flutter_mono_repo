682b0933076695659b8146a659a2ed56adae3509
Panachai <panachai.ny@gmail.com>
2023-08-01 14:23:33 +0700

    clean & add melos

--- lib/flutter_mono_repo.dart
+++
@@ -1,4 +0,0 @@
-/// A Very Good Project created by Very Good CLI.
-library flutter_mono_repo;
-
-export 'src/flutter_mono_repo.dart';
--- lib/src/flutter_mono_repo.dart
+++
@@ -1,7 +0,0 @@
-/// {@template flutter_mono_repo}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class FlutterMonoRepo {
-  /// {@macro flutter_mono_repo}
-  const FlutterMonoRepo();
-}
