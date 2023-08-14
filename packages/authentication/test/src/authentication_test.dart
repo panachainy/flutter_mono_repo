3c1010d3da06fdf540c71a29bfa15e819a40aefe
Panachai <panachai.ny@gmail.com>
2023-08-14 13:34:59 +0700

    add auth package

---
+++ packages/authentication/test/src/authentication_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:authentication/authentication.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Authentication', () {
+    test('can be instantiated', () {
+      expect(Authentication(), isNotNull);
+    });
+  });
+}
